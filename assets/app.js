da161eab2dff4a735fd330ccdff0a88934733cd8
christianacca <ghp@christianacca.e4ward.com>
2014-08-30 17:40:58 +0100

    demo(autorefresh): initial commit

---
+++ assets/app.js
@@ -0,0 +1,130 @@
+angular.module('cc.autorefresh.demo', ['cc.autorefresh', 'ngMockE2E', 'ui.bootstrap', 'plunker', 'ngTouch'], function($httpProvider){
+  FastClick.attach(document.body);
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}).run(['$location', function($location){
+  //Allows us to navigate to the correct element on initialization
+/*
+  if ($location.path() !== '' && $location.path() !== '/') {
+    smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
+      location.replace(el.id);
+    });
+  }
+*/
+}])
+.factory('delayHTTP', function ($q, $timeout) {
+    // interceptor that will fake delays to backend
+    return {
+        request: function (request) {
+            var delayedResponse = $q.defer();
+            $timeout(function () {
+                delayedResponse.resolve(request);
+            }, 1000);
+            return delayedResponse.promise;
+        },
+        response: function (response) {
+            var deferResponse = $q.defer();
+
+            if (response.config.timeout && response.config.timeout.then) {
+                response.config.timeout.then(function () {
+                    deferResponse.reject({ status: 0});
+                });
+            } else {
+                deferResponse.resolve(response);
+            }
+
+            return $timeout(function () {
+                deferResponse.resolve(response);
+                return deferResponse.promise;
+            });
+        }
+    };
+})
+.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('delayHTTP');
+}])
+.run(function($httpBackend) {
+
+    // whitelist backend calls
+    $httpBackend.whenGET(/^\/templates\//).passThrough();
+});
+
+var builderUrl = "http://50.116.42.77:3001";
+
+function MainCtrl($scope, $http, $document, $modal, orderByFilter) {
+  $scope.showBuildModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'buildModal.html',
+      controller: 'SelectModulesCtrl',
+      resolve: {
+        modules: function() {
+          return $http.get(builderUrl + "/api/bootstrap").then(function(response) {
+            return response.data.modules;
+          });
+        }
+      }
+    });
+  };
+
+  $scope.showDownloadModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'downloadModal.html',
+      controller: 'DownloadCtrl'
+    });
+  };
+}
+
+var SelectModulesCtrl = function($scope, $modalInstance, modules) {
+  $scope.selectedModules = [];
+  $scope.modules = modules;
+
+  $scope.selectedChanged = function(module, selected) {
+    if (selected) {
+      $scope.selectedModules.push(module);
+    } else {
+      $scope.selectedModules.splice($scope.selectedModules.indexOf(module), 1);
+    }
+  };
+
+  $scope.downloadBuild = function () {
+    $modalInstance.close($scope.selectedModules);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss();
+  };
+
+  $scope.download = function (selectedModules) {
+    var downloadUrl = builderUrl + "/api/bootstrap/download?";
+    angular.forEach(selectedModules, function(module) {
+      downloadUrl += "modules=" + module + "&";
+    });
+    return downloadUrl;
+  };
+};
+
+var DownloadCtrl = function($scope, $modalInstance) {
+  $scope.options = {
+    minified: true,
+    tpls: true
+  };
+
+  $scope.download = function (version) {
+    var options = $scope.options;
+
+    var downloadUrl = ['ui-bootstrap-'];
+    if (options.tpls) {
+      downloadUrl.push('tpls-');
+    }
+    downloadUrl.push(version);
+    if (options.minified) {
+      downloadUrl.push('.min');
+    }
+    downloadUrl.push('.js');
+
+    return downloadUrl.join('');
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss();
+  };
+};
---
+++ ref-docs/js/docs-setup.js
@@ -0,0 +1,80 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "cc.autorefresh",
+      "shortName": "cc.autorefresh",
+      "type": "overview",
+      "moduleName": "cc.autorefresh",
+      "shortDescription": "Collection of directives and controllers for scheduling a routine to run at intervals",
+      "keywords": "api autorefresh button cancel cc ccautorefreshbtn ccautorefreshfn ccautorefreshfnctl class controller controllers create custom directive directives example extends extension gui intervals lead long overview pause refresh regular require routine scenarios scheduling trigger user"
+    },
+    {
+      "section": "api",
+      "id": "cc.autorefresh.ccAutoRefreshBtn",
+      "shortName": "cc.autorefresh.ccAutoRefreshBtn",
+      "type": "overview",
+      "moduleName": "cc.autorefresh.ccAutoRefreshBtn",
+      "shortDescription": "Button directive for scheduling a routine to run at intervals.",
+      "keywords": "api autorefresh button cc ccautorefreshbtn ccautorefreshfn directive intervals overview routine scheduling"
+    },
+    {
+      "section": "api",
+      "id": "cc.autorefresh.ccAutoRefreshBtn.directive:ccAutoRefreshBtn",
+      "shortName": "ccAutoRefreshBtn",
+      "type": "directive",
+      "moduleName": "cc.autorefresh.ccAutoRefreshBtn",
+      "shortDescription": "Schedule a expression to run every n number of milliseconds and assign the return value to the scope.",
+      "keywords": "accepts angular api assign autorefresh button cancel cc ccautorefreshbtn ccautorefreshfn data-bind default demand details determines directive executed execution expression extends interval milliseconds number overrides parameters pause progress refresh refreshfn refreshinterval refreshmodel refreshtranslations return returned returns schedule scope translations user"
+    },
+    {
+      "section": "api",
+      "id": "cc.autorefresh.ccAutoRefreshBtn.type:ccAutoRefreshDefaultTranslations",
+      "shortName": "ccAutoRefreshDefaultTranslations",
+      "type": "object",
+      "moduleName": "cc.autorefresh.ccAutoRefreshBtn",
+      "shortDescription": "Default translations for the ccAutoRefreshBtn",
+      "keywords": "api autorefresh cc ccautorefreshbtn default directive object translations type"
+    },
+    {
+      "section": "api",
+      "id": "cc.autorefresh.ccAutoRefreshFn",
+      "shortName": "cc.autorefresh.ccAutoRefreshFn",
+      "type": "overview",
+      "moduleName": "cc.autorefresh.ccAutoRefreshFn",
+      "shortDescription": "Directive for scheduling a routine to run at intervals.",
+      "keywords": "api autorefresh cc ccautorefreshfn directive intervals overview routine scheduling"
+    },
+    {
+      "section": "api",
+      "id": "cc.autorefresh.ccAutoRefreshFn.controller:ccAutoRefreshFnCtl",
+      "shortName": "ccAutoRefreshFnCtl",
+      "type": "controller",
+      "moduleName": "cc.autorefresh.ccAutoRefreshFn",
+      "shortDescription": "ccAutoRefreshFnCtl provides the API for the cc-auto-refresh-fn directive. The controller exposes services",
+      "keywords": "api autorefresh callback cancelrefresh cancels cc cc-auto-refresh-fn cc-autorefresh-ccautorefreshfn-controller-ccautorefreshfnctl-page cc-autorefresh-ccautorefreshfn-controller-page cc-autorefresh-fn ccautorefreshfn ccautorefreshfnctl class considered controller convenient create currently custom directive directives disabled executes executing exposes expression extend field function gui identifies interval isdisabled ispaused isrefreshing isvisible method paused pauses refresh refresh-fn refresh-model refresh-on-refreshed return schedule scope services sets supplied togglepause visible"
+    },
+    {
+      "section": "api",
+      "id": "cc.autorefresh.ccAutoRefreshFn.directive:ccAutoRefreshFn",
+      "shortName": "ccAutoRefreshFn",
+      "type": "directive",
+      "moduleName": "cc.autorefresh.ccAutoRefreshFn",
+      "shortDescription": "Schedule a expression to run every n number of milliseconds and assign the return value to the scope.",
+      "keywords": "$destroy $http $q $scope adding angular angularjs api argument assign assigned asynchronous autorefresh call called cancellationtoken cancelling cc ccautorefreshfn ccautorefreshfnctl completed completion config controller creates current data-bind determines directive ea eventargs example exception executed execution exposes expression field gui https identified ignored in-flight interval milliseconds model number occurring org parameter paused progress promise property reason receive references refresh refreshbusy refreshfn refreshinterval refreshmodel refreshonrefreshed refreshpaused request required resolved return returned returns safely schedule scope set successful supplied switches timeout true truthy user view"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/cc.autorefresh",
+  "scripts": [
+    "angular.min.js",
+    "cc-autorefresh-tpls-0.1.0.js"
+  ]
+};
